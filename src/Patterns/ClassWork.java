97ea6712c518dcbeb9db40545556cc12b9214a22
Dell <roshan123gupta0@gmail.com>
2024-11-13 11:45:25 +0530

    done

--- src/Patterns/ClassWork.java
+++ src/Patterns/ClassWork.java
@@ -1,5 +1,16 @@
 package Patterns;
 
 public class ClassWork {
+	public static void main(String[] args) {
+		for(int row =1; row<=5; row++) {
+			for(int col=1; col=row;col++) {
+				System.out.print("+");
+			}
+			System.out.println();
+		}
+	}
+	
+	
 
 }
+
